233436ff429b0d249577d06b8870c46a3d4f66e1
leandro <l34ndr0penha@gmail.com>
2023-08-04 22:17:48 -0300

    construindo navegacao

--- js/app.js
+++ js/app.js
@@ -1,33 +1,34 @@
-const charsContainer = document.querySelector('.chars-container')
-const searchInput = document.querySelector('#search')
+const charsContainer = document.querySelector(".chars-container");
+const searchInput = document.querySelector("#search");
 
-const speciesFilter = document.querySelector('#species')
-const genderFilter = document.querySelector('#gender')
-const statusFilter = document.querySelector('#status')
-const loadMoreButton =  document.querySelector('#load-more')
+const speciesFilter = document.querySelector("#species");
+const genderFilter = document.querySelector("#gender");
+const statusFilter = document.querySelector("#status");
+const loadMoreButton = document.querySelector("#load-more");
 
-const API = 'https://rickandmortyapi.com/api'
+const API = "https://rickandmortyapi.com/api";
 
 const defaultFilters = {
-    name: '',
-    species: '',
-    gender: '',
-    status: '',
-    page: 1
-}
-
+  name: "",
+  species: "",
+  gender: "",
+  status: "",
+  page: 1,
+};
 
 async function getCharacters({ name, species, gender, status, page = 1 }) {
-    const response = await fetch(`${API}/character?name=${name}&species=${species}&gender=${gender}&status=${status}&page=${page}`)
+  const response = await fetch(
+    `${API}/character?name=${name}&species=${species}&gender=${gender}&status=${status}&page=${page}`
+  );
 
-    const characters = await response.json()
+  const characters = await response.json();
 
-    return characters.results
+  return characters.results;
 }
 
 async function render({ characters }) {
-    characters.forEach((characters) => {
-        return charsContainer.innerHTML += `
+  characters.forEach((characters) => {
+    return (charsContainer.innerHTML += `
         <div class="char">
         <img src="${characters.image}" alt="">
         <div class="char-info">
@@ -35,54 +36,49 @@ async function render({ characters }) {
           <span>${characters.species}</span>
         </div>
       </div>
-        `
-    })
+        `);
+  });
 }
 
-function handleFilterChange(type, e){
-    return async () => {
-        defaultFilters[type] = e.target.value
-        charsContainer.innerHTML = ''
-        const characters = await getCharacters(defaultFilters)
-        render({ characters })
-    }
-
+function handleFilterChange(type, e) {
+  return async () => {
+    defaultFilters[type] = e.target.value;
+    charsContainer.innerHTML = "";
+    const characters = await getCharacters(defaultFilters);
+    render({ characters });
+  };
 }
 
-async function handleLoadMore () {
-    defaultFilters.page += 1
-        const characters = await getCharacters(defaultFilters)
-        render({characters})
+async function handleLoadMore() {
+  defaultFilters.page += 1;
+  const characters = await getCharacters(defaultFilters);
+  render({ characters });
 }
 
 function addListeners() {
+  speciesFilter.addEventListener("change", async (e) => {
+    handleFilterChange("species", e)();
+  });
 
-    speciesFilter.addEventListener('change', async (e) => {
-       handleFilterChange('species', e)()
-    })
+  genderFilter.addEventListener("change", async (e) => {
+    handleFilterChange("gender", e)();
+  });
 
-    genderFilter.addEventListener('change', async (e) => {
-        handleFilterChange('gender', e)()
-    })
+  statusFilter.addEventListener("change", async (e) => {
+    handleFilterChange("status", e)();
+  });
 
-    statusFilter.addEventListener('change', async (e) => {
-        handleFilterChange('status', e)()
-    })
+  searchInput.addEventListener("keyup", async (e) => {
+    handleFilterChange("name", e)();
+  });
 
-    searchInput.addEventListener('keyup', async (e) => {
-        handleFilterChange('name', e)()
-    })
-
-    loadMoreButton.addEventListener('click', handleLoadMore)
+  loadMoreButton.addEventListener("click", handleLoadMore);
 }
-
-
-
 
 async function main() {
-    const characters = await getCharacters(defaultFilters)
-    addListeners()
-    render({ characters })
+  const characters = await getCharacters(defaultFilters);
+  addListeners();
+  render({ characters });
 }
 
-main()
+main();
